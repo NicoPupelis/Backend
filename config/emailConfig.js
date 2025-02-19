e553c80da810fc6c8b4abc28154b4581a6e934d3
NicoPupelis <132392870+NicoPupelis@users.noreply.github.com>
2025-02-19 10:16:50 -0300

    Update emailConfig.js

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -3,14 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false, // Cambiado a `false` porque Gmail usa STARTTLS en el puerto 587
+    secure: false, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
 });
 
-// Verificar conexión con Gmail SMTP
+
 transporter.verify((error, success) => {
     if (error) {
         console.error("❌ Error en la conexión con Gmail SMTP:", error);
