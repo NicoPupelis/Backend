e458c9fea1e3c8e9f362a1ad9bfad725623076e2
NicoPupelis <132392870+NicoPupelis@users.noreply.github.com>
2025-02-19 10:17:56 -0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 
 const router = express.Router();
 
-// Ruta protegida: Obtener datos del usuario autenticado
+
 router.get("/me", authMiddleware, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select("-password");
